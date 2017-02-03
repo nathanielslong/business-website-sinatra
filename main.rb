c73e224f5693832a2bec2b78457ce048f3772b23
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-03 11:01:28 -0500

    initialized basic structure

---
+++ main.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sendgrid-ruby'
+
+def email_sending
+  user_email = params["email"]
+  user_name = params["name"]
+  user_body = params["message"]
+  from = SendGrid::Email.new(email: user_email)
+  subject = 'Hello #{user_name}'
+  to = SendGrid::Email.new(email: 'test@example.com')
+  content = SendGrid::Content.new(type: 'text/plain', value: user_body)
+  mail = SendGrid::Mail.new(from, subject, to, content)
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  p response.status_code
+  p response.body
+end
+
+get "/" do
+  erb :home
+end
+
+post "/contact-us" do
+  p params
+  email_sending
+end
