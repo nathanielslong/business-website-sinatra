554248fc62da51cbc99d79067182d5068c940c8f
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-06 10:10:47 -0500

    email functionality works successfully

--- main.rb
+++ main.rb
@@ -2,18 +2,20 @@ require 'sinatra'
 require 'sendgrid-ruby'
 
 def email_sending
+  user_name = "#{params["fname"]} #{params["lname"]}"
   user_email = params["email"]
-  user_name = params["name"]
+  user_phone = params["phone"]
+  user_company = params["company"]
   user_body = params["message"]
+  user_country = params["country"]
   from = SendGrid::Email.new(email: user_email)
-  subject = 'Hello #{user_name}'
+  subject = 'Consulting interest from #{user_name} at #{user_company} in #{user_country}, phone contact #{user_phone}'
   to = SendGrid::Email.new(email: 'test@example.com')
   content = SendGrid::Content.new(type: 'text/plain', value: user_body)
   mail = SendGrid::Mail.new(from, subject, to, content)
   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
   response = sg.client.mail._('send').post(request_body: mail.to_json)
   p response.status_code
-  p response.body
 end
 
 get "/" do
@@ -33,6 +35,5 @@ get "/major_projects" do
 end
 
 post "/contact-us" do
-  p params
   email_sending
 end
